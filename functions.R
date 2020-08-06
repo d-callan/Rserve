4997a0a944cae00a669510e573128398f2d4df6f
d-callan <dmleake92@gmail.com>
2020-08-06 10:21:10 -0400

    first commit

---
+++ functions.R
@@ -0,0 +1,3 @@
+test <- function(x) {
+  log(x)
+}
---
+++ run_rserve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+CONFIG_PATH=${RSERVE_HOME}/etc/Rserve.conf
+
+R \
+-e 'library(stats)' \
+-e 'library(data.table)' \
+-e 'library(Rserve)' \
+-e 'source("functions.R")' \
+-e 'run.Rserve(debug = TRUE, config.file = "'${CONFIG_PATH}'")'
